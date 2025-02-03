1624d4f748742f2db5adbac417dd22283fdf717c
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2025-02-03 16:59:34 +0200

    fix lint errors 1

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
@@ -15,7 +16,8 @@ export default tseslint.config(
     },
     plugins: {
       "react-hooks": reactHooks,
-      "react-refresh": reactRefresh
+      "react-refresh": reactRefresh,
+      prettier: prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
--- src/helpers/generateHashId.ts
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from "uuid";
-
-export const generateHashId = (): string => {
-  const uuid = uuidv4().replace(/-/g, "");
-
-  const length = Math.random() * (20 - 9) + 9;
-
-  return uuid.slice(0, length);
-};
